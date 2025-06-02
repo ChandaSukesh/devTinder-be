0b8ba576b9047c5091ac577d1e2f3284f24c3097
Chanda Sukesh <chandasukesh@gmail.com>
2025-06-02 17:52:11 +0530

    advance url patterns

--- src/httpmethods.js
+++ src/httpmethods.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 // This below use wil get print but not get as the order matters,
-app.use("/user", (req, res) => {
-  res.send("This gets print");
-});
+// app.use("/user", (req, res) => {
+//   res.send("This gets print");
+// });
 
 app.get("/user", (req, res) => {
   res.send({
@@ -17,6 +17,26 @@ app.get("/user", (req, res) => {
 app.post("/postUser", (req, res) => {
   res.send("This is post call!");
 });
+
+// some advance concepts with route
+// 1. here  ? refers to optional, which means b is optional here
+// ex: /ab?c===> /ac if we give also it wil work in the url
+// ex: /ab(cd)?d ===? here cd is optional, /abd is allowed
+
+// 2. + refers to more than 1 occurance also allowed
+// ex: /ab+c===> /abbbbc is allows, but not /abcc
+
+// 3. * refers anything is allowed in the place of *
+// ex: /ab*c ===> /abSukeshhdhdhc
+
+//4. regex also allowed instead of string
+// ex: /a/
+// app.get("/ab+c", (req, res) => {
+//   res.send({
+//     firstname: "Chanda",
+//     lastName: "Sukesh",
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("app is listening");
