2422e085422b73d34ac13c443c8205aef4ecff65
Chanda Sukesh <chandasukesh@gmail.com>
2025-07-01 17:07:51 +0530

    all basic api methods with mongoose

--- src/app.js
+++ src/app.js
@@ -32,6 +32,53 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//get user by email
+app.get("/user", async (req, res) => {
+  console.log("suk1", req.body.emailId);
+  try {
+    const userEmailId = await User.find({ emailId: req.body.emailId });
+    console.log("suk2", userEmailId);
+    res.send(userEmailId);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// get api to get all the users
+app.get("/feed", async (req, res) => {
+  try {
+    // the find method is one the method of mongoose, see mongoose documentation for 'Model' for other methods
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//delete the user
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    // await User.findByIdAndDelete({ _id: userId }); or await User.findByIdAndDelete(userId );
+    // both of them works
+    const users = await User.findByIdAndDelete({ _id: userId });
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// update the user
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const users = await User.findByIdAndUpdate({ _id: userId }, req.body);
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 // here always first data connection should happen first later server should start listening
 // this is  standard rule
 connectDB()
@@ -44,3 +91,6 @@ connectDB()
   .catch((err) => {
     console.error("Data error!!", err);
   });
+
+// Note:
+// 1)any other data which is not present in schema, then it wont get added when you send from postman(i.e.., field)
